b232c1d32dd66afbd764cf6b3810fc0f057145a9
Konrad44444 <konrad_warzecha@wp.pl>
2024-03-20 11:01:39 +0100

    graph edges for v2

--- app/src/components/cpm/LoadGraph.tsx
+++ app/src/components/cpm/LoadGraph.tsx
@@ -2,6 +2,7 @@ import { useLoadGraph } from "@react-sigma/core";
 import CriticalPath from "../../interfaces/CriticalPath";
 import { useEffect } from "react";
 import Graph from "graphology";
+import isActivityV1 from "../../utils/checkInterface";
 
 interface Props {
   nodes: CriticalPath;
@@ -21,6 +22,18 @@ const LoadGraph = ({ nodes }: Props) => {
         size: 20,
         color: nodes.criticalNodes.includes(index) ? "#bd090c" : "#0328fc",
       });
+    });
+
+    nodes.activites.map((input, index) => {
+      if (isActivityV1(input)) {
+      } else {
+        graph.addEdge(input.afterEffect[0] - 1, input.afterEffect[1] - 1, {
+          color: nodes.criticalActivites.includes(index)
+            ? "#bd090c"
+            : "#0328fc",
+          label: `name: ${input.name} | time: ${input.time}`,
+        });
+      }
     });
 
     loadGraph(graph);
